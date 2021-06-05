b1ba17cbea8f7ca71fbf4e8122bbe93ec9edc7cb
ЧайныйПакетик#243 <41287886+ArditStory@users.noreply.github.com>
2021-06-05 15:32:11 +0500

    Update weather.js

--- commands/weather.js
+++ commands/weather.js
@@ -19,6 +19,8 @@ module.exports = {
 
         }
         else {
+          if (args[0] === 'start') {
+            
 
 
             var temp = Math.floor(Math.random()*20 - 10) 
@@ -80,9 +82,86 @@ module.exports = {
             { name: 'Погода на день:', value: `${osad}`, inline: true },
             { name: 'Влажность воздуха:', value: `${vlvoz}%`, inline: true },
         )
+ 
+             global.intervalid = setInterval(()=>{
+              message.channel.send(weather); // the `TextChannel` class
+            },1000*60*60*24) // setting the time to 24 hours in ms
+
+
+          }
+
+        }
+        if (args[0] === 'break') {
+          clearInterval(global.intervalid);
+          console.log("ОСТАНОВЛЕНО!")
+        }
+        if (args[0] === 'once') {
+
+
+
+          var temp = Math.floor(Math.random()*20 - 10) 
+          
+          if (temp > 0) {
+            var name = new Array( // Объявление массива name и занесение в него большого количества имён
+              'Дождь',
+              'Дождь с грозой',
+              'Проливной дождь',
+              'Ветер',
+              'Гроза',
+              'Град',
+              'Дождь с градом',
+              'Пасмурно',
+              'Солечно',
+              'Солечно',
+              'Переменная облачность',
+              'Облачно',
+              'Туман'
+        
+              
+          
+          );
+          } else {
+            var name = new Array( // Объявление массива name и занесение в него большого количества имён
+              'Ветер',
+              'Град',
+              'Пасмурно',
+              'Солечно',
+              'Переменная облачность',
+              'Облачно',
+              'Туман',
+              'Снег',
+              'Изморозь',
+              'Снежная буря'
+        
+              
+          
+          );
+          }
+
+          var osad = name[Math.floor(Math.random()*(name.length))];
+          var veter = Math.floor(Math.random()*14)
+          var vlvoz = Math.floor(Math.random()*98)
+          if (vlvoz < 50) {
+            vlvoz = vlvoz + 50
+          }
+
+
+          const weather = new Discord.MessageEmbed()
+          .setColor('#0099ff')
+          .setAuthor(`${message.author.username}`, `${message.author.displayAvatarURL({ dynamic: true })}`)
+          .setTitle('Погода на день:')
+          .setThumbnail('https://icons.iconarchive.com/icons/papirus-team/papirus-apps/512/weather-icon.png')
+          .addFields(
+            { name: 'Температура воздуха:', value: `${temp}°С` },
+            { name: 'Ветер:', value: `${veter} м/с` },
+            { name: 'Погода на день:', value: `${osad}`, inline: true },
+            { name: 'Влажность воздуха:', value: `${vlvoz}%`, inline: true },
+        )
+
         message.channel.send(weather)
 
-
+          
+          console.log("ОСТАНОВЛЕНО!")
         }
 		
 				
